41e540d2d87f4e308523a41364e29f01fc1d92be
hgbeleza <developer.beleza06@gmail.com>
2024-04-28 19:11:30 -0300

    init project + header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular_17_crud_localstorage';
-}
+export class AppComponent {}
