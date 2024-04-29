2d963e5142d8c31ccb9ea9231171d6da70bf5020
hgbeleza <developer.beleza06@gmail.com>
2024-04-29 06:27:38 -0300

    organizing folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './components/container/container.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SeparatorComponent } from './components/separator/separator.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FormsModule } from '@angular/forms';
-
-interface Contact {
-  id: number;
-  nome: string;
-  telefone: string;
-}
-
-import agenda from './contact.json';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
 import { ContactFormComponent } from './pages/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ContainerComponent,
-    HeaderComponent,
-    SeparatorComponent,
-    ContactComponent,
-    FormsModule,
-    ContactFormComponent
-  ],
+  imports: [ContactListComponent, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
-  contacts: Contact[] = agenda;
 
-  filtroPorTexto: string = '';
-
-  filtrarContatosPorTexto(): Contact[] {
-    if (!this.filtroPorTexto) {
-      return this.contacts;
-    }
-    return this.contacts.filter((contact) => {
-      return contact.nome
-        .toLowerCase()
-        .includes(this.filtroPorTexto.toLowerCase());
-    });
-  }
-
-  filtrarContatosPorLetraInicial(letra: string): Contact[] {
-    return this.filtrarContatosPorTexto().filter((contact) => {
-      return contact.nome.toLocaleLowerCase().startsWith(letra);
-    });
-  }
 }
---
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/container/container.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SeparatorComponent } from '../../components/separator/separator.component';
+import { ContactComponent } from '../../components/contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
+interface Contact {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from '../../contact.json';
+
+@Component({
+  selector: 'app-contact-list',
+  standalone: true,
+  imports: [
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+    ContactComponent,
+    FormsModule,
+    ContactFormComponent
+  ],
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contacts: Contact[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contact[] {
+    if (!this.filtroPorTexto) {
+      return this.contacts;
+    }
+    return this.contacts.filter((contact) => {
+      return contact.nome
+        .toLowerCase()
+        .includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contact[] {
+    return this.filtrarContatosPorTexto().filter((contact) => {
+      return contact.nome.toLocaleLowerCase().startsWith(letra);
+    });
+  }
+}
